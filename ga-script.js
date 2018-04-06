95dc3055c862ac4b5ce1286d546decb215c660e8
quochunguit <36918903+quochunguit@users.noreply.github.com>
2018-04-06 08:20:15 +0700

    Add files via upload

---
+++ ga-script.js
@@ -0,0 +1,8 @@
+
+let gaEventActionInput1 = document.getElementById('gaEventActionInput');
+let gaEventCategoryInput = document.getElementById('gaEventCategoryInput');
+let gaEventLabelInput = document.getElementById('gaEventLabelInput');
+let gaEventValueInput = document.getElementById('gaEventValueInput');
+
+//let generateGaTrackingCode = `gtag('event', ${gaEventActionInput}, {'event_category': ${gaEventCategoryInput}, 'event_label': ${gaEventLabelInput}, 'value': ${gaEventValueInput}});` ;
+
